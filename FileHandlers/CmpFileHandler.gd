2d167416594252883f865cfcc47c4a48b068d64a
Stephen Diniz <DizzyThermal@gmail.com>
2025-04-07 20:28:01 -0400

    Syncing FileHandlers

--- FileHandlers/CmpFileHandler.gd
+++ FileHandlers/CmpFileHandler.gd
@@ -2,23 +2,23 @@ class_name CmpFileHandler extends NTK_FileHandler
 
 const CmpTile = preload("res://DataTypes/CmpTile.gd")
 
-var map_id := -1
 var map_name := ""
 var width := 0
 var height := 0
 var tiles: Array[CmpTile] = []
 
-func _init(
-		map_id: int) -> void:
-	super(("TK%06d.cmp" % map_id), Resources.map_dir)
-	self.map_id = map_id
-	file_position = 4  # CMAP
+func _init(file_path):
+	super(file_path)
 
-	var dims := read_u32()
-	width = dims & 0x0000FFFF
-	height = dims >> 0x10
+	var file_position: int = 4  # CMAP
 
-	var compressed_data := read_bytes(file_size - 4)
+	var dims := read_u32(file_position)
+	file_position += 4
+	self.width = dims & 0x0000FFFF
+	self.height = dims >> 0x10
+
+	var compressed_data := read_bytes(file_position, file_size - 4)
+	file_position += file_size - 4
 	var map_data := compressed_data.decompress_dynamic(width * height * 6, FileAccess.COMPRESSION_DEFLATE)
 
 	for i in range(int(len(map_data) / 6)):
@@ -29,6 +29,48 @@ func _init(
 			map_data.decode_u16(idx + 4) - 1
 		))
 
-	if FileAccess.file_exists(Resources.mnm_dir + ("/%06d.mnm" % map_id)):
-		var mnm := MnmFileHandler.new(map_id)
-		map_name = mnm.map_name
+func update_map(map_width: int, map_height: int, map_tiles: Array) -> void:
+	tiles.clear()
+	self.width = map_width
+	self.height = map_height
+	for y in range(self.height):
+		for x in range(self.width):
+			tiles.append(CmpTile.new(
+				map_tiles[y][x]["ab_index"],
+				int(map_tiles[y][x]["unpassable"]),
+				map_tiles[y][x]["sobj_index"],
+			))
+
+func save_to_file(file_path: String) -> void:
+	if DirAccess.dir_exists_absolute(file_path):
+		DirAccess.remove_absolute(file_path)
+
+	var map_file_access := FileAccess.open(file_path, FileAccess.ModeFlags.WRITE)
+	
+	map_file_access.store_8(67)	# C
+	map_file_access.store_8(77)	# M
+	map_file_access.store_8(65)	# A
+	map_file_access.store_8(80)	# P
+
+	# Dimensions
+	map_file_access.store_16(self.width)
+	map_file_access.store_16(self.height)
+
+	# Collect Map Data
+	var map_data: PackedByteArray = PackedByteArray()
+	map_data.resize(len(tiles) * 6)
+	var map_data_pointer := 0
+	for tile in tiles:
+		if tile.ab_index == 65535:
+			print()
+		map_data.encode_u16(map_data_pointer, max(tile.ab_index, 0))
+		map_data.encode_u16(map_data_pointer + 2, int(tile.unpassable_tile))
+		map_data.encode_u16(map_data_pointer + 4, tile.sobj_index + 1)
+		map_data_pointer += 6
+
+	# Deflate Map Data
+	var compressed_map_data := map_data.compress(FileAccess.COMPRESSION_DEFLATE)
+	map_file_access.store_buffer(compressed_map_data)
+
+	map_file_access.flush()
+	map_file_access.close()
--- FileHandlers/MnmFileHandler.gd
+++ FileHandlers/MnmFileHandler.gd
@@ -2,7 +2,6 @@ class_name MnmFileHandler extends NTK_FileHandler
 
 var MNM_NAME_SIZE := 0x1F
 
-var map_id := -1
 var map_name := ""
 
 var npc_count := 0
@@ -11,19 +10,19 @@ var npcs := []
 var warp_count := 0
 var warps := []
 
-func _init(
-		map_id: int) -> void:
-	super(("%06d.mnm" % map_id), Resources.mnm_dir)
-	self.map_id = map_id
+func _init(file_path):
+	super(file_path)
 
 	# Seek into the JPEG
-	file_position = 0x300
+	var file_position: int = 0x300
 
 	# Find end of JPEG
 	while true:
-		var byte = read_u8()
+		var byte = read_u8(file_position)
+		file_position += 1
 		if byte == 0xFF:
-			byte = read_u8()
+			byte = read_u8(file_position)
+			file_position += 1
 			if byte == 0xD9:
 				# End of JPEG (FF D9)
 				break
@@ -32,15 +31,20 @@ func _init(
 				file_position -= 1
 
 	# Map Name Length
-	var map_name_length = read_s32()
+	var map_name_length = read_s32(file_position)
+	file_position += 4
 	map_name = read_utf16(map_name_length)
 
 	# NPCs
-	npc_count = read_s32()
+	npc_count = read_s32(file_position)
+	file_position += 4
 	for i in range(npc_count):
-		var npc_name = read_utf16(MNM_NAME_SIZE)
-		var npc_map_x = read_s32()
-		var npc_map_y = read_s32()
+		var npc_name = read_utf16(file_position, MNM_NAME_SIZE)
+		file_position += MNM_NAME_SIZE
+		var npc_map_x = read_s32(file_position)
+		file_position += 4
+		var npc_map_y = read_s32(file_position)
+		file_position += 4
 		npcs.append({
 			'name': npc_name,
 			'x_coord': npc_map_x,
@@ -48,16 +52,23 @@ func _init(
 		})
 
 	# Warp
-	warp_count = read_s32()
+	warp_count = read_s32(file_position)
+	file_position += 4
 	for i in range(warp_count):
-		var warp_map_id = read_s32()
-		var warp_map_name = read_utf16(MNM_NAME_SIZE)
-		var warp_x = read_s32()
-		var warp_y = read_s32()
-		var warp_npc_count = read_s32()
+		var warp_map_id = read_s32(file_position)
+		file_position += 4
+		var warp_map_name = read_utf16(file_position, MNM_NAME_SIZE)
+		file_position += MNM_NAME_SIZE
+		var warp_x = read_s32(file_position)
+		file_position += 4
+		var warp_y = read_s32(file_position)
+		file_position += 4
+		var warp_npc_count = read_s32(file_position)
+		file_position += 4
 		var warp_npcs := []
 		for j in range(warp_npc_count):
-			var warp_npc_name = read_utf16(MNM_NAME_SIZE)
+			var warp_npc_name = read_utf16(file_position, MNM_NAME_SIZE)
+			file_position += MNM_NAME_SIZE
 			warp_npcs.append(warp_npc_name)
 
 		warps.append({
